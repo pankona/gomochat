ff574ba77f66a3c6b29ed5c45a8bd6a5d8e01a61
pankona <yosuke.akatsuka@gmail.com>
2018-02-21 10:42:24 +0900

    migrate java to kotlin

---
+++ android/app/src/main/java/io/pankona/gomochat/MainActivity.kt
@@ -0,0 +1,29 @@
+package io.pankona.gomochat
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import gomochat.*
+
+class MainActivity : AppCompatActivity(), ReceiveMessageListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val client = Gomochat.newClient()
+        client.addReceiveMessageListener(this)
+
+        try {
+            client.connect("127.0.0.1", 8080)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+    }
+
+    override fun onReceiveMessage(msg: String) {
+        // TODO: implement
+
+    }
+}
