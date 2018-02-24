6038c327eeebbb7d12394e99a71fa99775236e5d
pankona <yosuke.akatsuka@gmail.com>
2018-02-24 23:28:04 +0900

    add ui widget components and implement listeners

--- android/app/src/main/java/io/pankona/gomochat/MainActivity.kt
+++ android/app/src/main/java/io/pankona/gomochat/MainActivity.kt
@@ -2,17 +2,37 @@ package io.pankona.gomochat
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 import gomochat.*
 
-class MainActivity : AppCompatActivity(), ReceiveMessageListener {
+class MainActivity : AppCompatActivity(), ReceiveMessageListener, View.OnClickListener {
+
+    lateinit var log: TextView
+    lateinit var name: EditText
+    lateinit var message: EditText
+    lateinit var send: Button
+    lateinit var client: Client
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val client = Gomochat.newClient()
+        log = this.findViewById(R.id.log) as TextView
+        name = this.findViewById(R.id.name) as EditText
+        message = this.findViewById(R.id.message) as EditText
+        send = this.findViewById(R.id.send) as Button
+        send.setOnClickListener(this)
+
+        client = Gomochat.newClient()
         client.addReceiveMessageListener(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
 
         try {
             client.connect("127.0.0.1", 8080)
@@ -23,5 +43,13 @@ class MainActivity : AppCompatActivity(), ReceiveMessageListener {
 
     override fun onReceiveMessage(msg: String) {
         // TODO: implement
+        log.append("$msg\n")
+    }
+
+    override fun onClick(v: View) {
+        val n = name.text
+        val m = message.text
+
+        client.sendMessage("$n: $m")
     }
 }
